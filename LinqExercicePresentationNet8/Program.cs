1f2d55f7a9e9b23e46bf7c5f903cec9012a3e939
pedro <prowkek@laposte.net>
2025-10-08 09:43:39 +0200

    ajout de choix d'util

--- LinqExercicePresentationNet8/Program.cs
+++ LinqExercicePresentationNet8/Program.cs
@@ -2,4 +2,27 @@
 
 ExerciceConverter converter = new ExerciceConverter();
 
-converter.ConvertCSVToJSON("../../../SourceExample.csv", "../../../output.json", "", true);
+Console.Write("Saisissez l'emplacement du fichier CSV à convertir (chemin absolu) : ");
+string? sourcePath = Console.ReadLine();
+
+Console.Write("Saisissez l'emplacement du fichier JSON converti (chemin absolu) : ");
+string? destPath = Console.ReadLine();
+
+Console.Write("Voulez-vous rechercher un élément en particulier dans le fichier ? (O/N)");
+string? choice = Console.ReadLine();
+string? stringToSearch = "";
+if (choice.ToUpper() == "O")
+{
+    Console.Write("Tapez votre texte à rechercher dans le fichier : ");
+    stringToSearch = Console.ReadLine();
+}
+
+Console.Write("Trier par ordre décroissant ? (O/N)");
+choice = Console.ReadLine();
+bool isDesc = false;
+if (choice.ToUpper() == "O")
+{
+    isDesc = true;
+}
+
+converter.ConvertCSVToJSON(sourcePath, destPath, stringToSearch, isDesc);
